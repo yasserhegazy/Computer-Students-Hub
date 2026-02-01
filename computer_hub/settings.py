ded904ef20d880653f7e801acb093d8413c59096
Yasser Hegazy <yasseranahegazy@gmail.com>
2026-02-01 20:50:46 +0200

    Add starting view for the project

--- computer_hub/settings.py
+++ computer_hub/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "computer_hub.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "computer_hub" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
